bfc2c3cac9aa555c12338fd4b4a10df6940c0399
dalibor <mail@daliborgogic.com>
2017-05-29 09:39:34 +0200

    don't minify css in dev

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ module.exports = {
       const styl = require('stylus')
       styl(str, opts).render(function (err, css) {
         if (err) throw err
-        ret = css.replace(/\s/g, '')
+        ret = css.replace(/\\s /g, '')
       })
-      return '<style>' + ret + '</style>'
+      return '\n<style>\n' + ret + '</style>'
     }
   },
   title: 'Lorem Ipsum'
