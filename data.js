b3e8923d40ba968dec7e964dc885d6aff4b7ba87
dalibor <mail@daliborgogic.com>
2017-06-04 21:23:13 +0200

    share meta tags

--- data.js
+++ data.js
@@ -12,6 +12,8 @@ module.exports = {
     }
   },
   brandColor: '#212121', // red #ff5252
+  description: 'DevOpsLinks is a free newsletter for DevOps & SysAdmins Featuring Curated Content, Must Read Articles & Tools.',
+  cardImage: 'https://placehold.it/1280x760',
   monospace: 'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;',
   sansSerif: '-apple-system, BlinkMacSystemFont, \'Segoe UI\', \'Roboto\', \'Oxygen\', \'Ubuntu\', \'Cantarell\', \'Fira Sans\', \'Droid Sans\', \'Helvetica Neue\', sans-serif'
 }
